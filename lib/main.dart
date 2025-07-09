446993a09f2a1a52a8a807b1d76acbfb1e32c3a4
shraddha666 <shraddhasa636@gmail.com>
2025-07-09 19:38:59 +0530

    Added AppBar to Inventory App

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Inventory app"),
+        ),
+      )
     );
   }
 }
