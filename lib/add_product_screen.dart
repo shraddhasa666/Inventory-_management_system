3e1653b51ffcaa68d00e2577fb597678df138b9b
shraddha666 <shraddhasa636@gmail.com>
2025-07-11 12:14:21 +0530

    added product form fields: name, quantity and price

--- lib/add_product_screen.dart
+++ lib/add_product_screen.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
 
-class AddProductScreen extends StatelessWidget {
+class AddProductScreen extends StatefulWidget {
   const AddProductScreen({super.key});
 
+  @override
+  State<AddProductScreen> createState() => _AddProductScreenState();
+}
+
+class _AddProductScreenState extends State<AddProductScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Product"),
       ),
-      body: Center(
-        child: Text("This  will be the product form"),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Product name"),
+            SizedBox(height: 8,),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter product name"
+              ),
+            ),
+            SizedBox(height: 16),
+            Text("Quantity"),
+            SizedBox(height: 8,),
+            TextField(
+              controller: _quantityController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter Quantity",
+              ),
+            ),
+            SizedBox(height: 16),
+        Text("Price"),
+        SizedBox(height: 8),
+        TextField(
+          controller: _priceController,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: "Enter price",
+           ),
+          ),
+          ],
+        ),),
     );
   }
 }
